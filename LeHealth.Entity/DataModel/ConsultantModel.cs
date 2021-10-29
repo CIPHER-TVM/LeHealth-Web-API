b7e146f59ccebd274673332fdab92d803cc6f7e2
NikhilNetroxe <51790243+NikhilNetroxe@users.noreply.github.com>
2021-10-29 10:21:19 +0530

    29102021

--- LeHealth.Entity/DataModel/ConsultantModel.cs
+++ LeHealth.Entity/DataModel/ConsultantModel.cs
@@ -4,18 +4,18 @@ using System.Text;
 
 namespace LeHealth.Entity.DataModel
 {
-   public  class ConsultantModel
+    public class ConsultantModel
     {
-    public string ConsultantName { get; set; }
+        public string ConsultantName { get; set; }
         public string FirstName { get; set; }
         public string MiddleName { get; set; }
         public string LastName { get; set; }
         public int DeptId { get; set; }
         public string AppType { get; set; }
-        public string ConsultantDate { get; set; } 
+        public string ConsultantDate { get; set; }
         public string Status { get; set; }
         public string ConsultantCode { get; set; }
         public int ConsultantId { get; set; }
 
-  }
+    }
 }
