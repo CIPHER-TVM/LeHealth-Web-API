dec72976c00f7e6789b2babdc6c3ef9afd79f46b
NikhilNetroxe <51790243+NikhilNetroxe@users.noreply.github.com>
2022-06-30 11:16:43 +0530

    Fixed EMR Module Missing Issues

--- LeHealth.Web/Controllers/EMR/EMRDefaultController.cs
+++ LeHealth.Web/Controllers/EMR/EMRDefaultController.cs
@@ -8,11 +8,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
 
 
 
